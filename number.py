b862f153e384a4334c34ef0e35f29336cc43a2e5
Venon282 <esto500@hotmail.com>
2025-02-03 16:51:30 +0100

    isPalindrome number add

--- number.py
+++ number.py
@@ -1,2 +1,10 @@
 def nextMultipleOf(n, multiple):
     return n if n % multiple == 0 else (n // multiple + 1) * multiple
+
+def isPalindrome(self, x):
+    """
+       :type x: int
+       :rtype: bool
+    """
+    x_str = str(x)
+    return x_str == x_str[::-1]
