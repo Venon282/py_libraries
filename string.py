3895936c51342a1c657ddf6b0319ef926eb8a618
Venon282 <esto500@hotmail.com>
2025-02-05 21:46:01 +0100

    is1SwapAreEqual add

--- string.py
+++ string.py
@@ -4,4 +4,27 @@ def prefixCount(words:list[str], pref:str):
         :type pref: str
         :rtype: int
         """
-        return sum([1 for word in words if word.startswith(pref)])
+    return sum([1 for word in words if word.startswith(pref)])
+
+def is1SwapAreEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) != len(s2):
+            return False
+
+        if s1 == s2:
+            return True
+
+        different = []
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                different.append(i)
+
+        if len(different) != 2:
+            return False
+
+        i, j = different
+        return s1[i] == s2[j] and s1[j] == s2[i]
