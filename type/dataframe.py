d844f9d95bcc7282fa6be2ee388d7a4f11dd3edf
THEVENON Esteban <ET281306@intra.cea.fr>
2026-09-18 10:45:52 +0200

    name replacement improved

--- type/dataframe.py
+++ type/dataframe.py
@@ -1,4 +1,9 @@
+from pathlib import Path
 import pandas as pd
+
+def _metaPath(path) -> Path:
+    p = Path(path)
+    return p.with_name(p.stem + "_meta.json")
 
 def saveMultiIndexCsv(df, path, **kwargs):
     n_row_levels = len(df.index[0]) if isinstance(df.index, pd.MultiIndex) else 1
@@ -8,10 +13,10 @@ def saveMultiIndexCsv(df, path, **kwargs):
     
     # Save metadata
     pd.Series({"n_row_levels": n_row_levels, "n_col_levels":  n_col_levels}) \
-    .to_json(path.replace(".csv", "_meta.json"))
+    .to_json(_metaPath(path))
     
 def loadMultiIndexCsv(path, **kwargs):
-    meta = pd.read_json(path.replace(".csv", "_meta.json"), typ="series")
+    meta = pd.read_json(_metaPath(path), typ="series")
     return pd.read_csv(path,
                     index_col=list(range(int(meta["n_row_levels"]))),
                     header=list(range(int(meta["n_col_levels"]))),
