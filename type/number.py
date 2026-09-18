0b1c0a625206560dfccea015e8a00e1b16ebbd6f
THEVENON Esteban <ET281306@intra.cea.fr>
2026-09-18 10:43:30 +0200

    romanToInt method to function

--- type/number.py
+++ type/number.py
@@ -27,7 +27,7 @@ def maxAscendingSum(nums):
 
 roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
 
-def romanToInt(self, s):
+def romanToInt(s):
     """
         :type s: str
         :rtype: int
